5463c89c4d568b65ef5ca007d48715070010aa7c
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2022-02-22 14:19:30 +0100

    Fixed #5

--- WPF_FirstProject/MainWindow.xaml.cs
+++ WPF_FirstProject/MainWindow.xaml.cs
@@ -72,8 +72,10 @@ namespace WPF_FirstProject
                     SolidColorBrush brush = ellipse.Fill as SolidColorBrush;
                     if (brush != null)
                     {
-                        string cleanCoordinate = ellipse.Name.Replace("n", "").Replace("_", ":");
-                        txbBlockTester.Text = $"{txbBlockTester.Text}\r\n{cleanCoordinate}"; 
+                        string cleanCoordinate = ellipse.Name.Replace("n", "");
+                        string[] sepCoorFromNumber = cleanCoordinate.Split("_");
+                        Debug.WriteLine(sepCoorFromNumber[0]);
+                        txbBlockTester.Text = $"{txbBlockTester.Text}\r\n{sepCoorFromNumber[0]}:{sepCoorFromNumber[1]}\t nr. {sepCoorFromNumber[2]}"; 
                         if (brush.Color == convertEmptyColor)
                         {
                             ellipse.Fill = new SolidColorBrush(convertColor);
@@ -107,6 +109,7 @@ namespace WPF_FirstProject
 
             txbBlockTester.Text = "";
 
+
             
         }
 
@@ -119,7 +122,10 @@ namespace WPF_FirstProject
             string selectItem = "";
             string selectItemFormat = "";
             string[] myArray = new string[0];
-            
+            int myLength;
+            int myWidth;
+            int ellipseCounter = 0;
+
             gMyGrid.Children.Clear();
             gLabelTest.Children.Clear();
 
@@ -142,101 +148,126 @@ namespace WPF_FirstProject
             Debug.WriteLine($"-_{selectItemFormat}_-");
             myArray = selectItemFormat.Split("x");
 
-            int myLength = int.Parse(myArray[0].Trim());
-            int myWidth = int.Parse(myArray[1].Trim());
-
-            
-
-            float totalSizeField = 840;
-            float EllipseDistance = 2F; //(float)(60.0 / EllipseSize)
-
-            if (myWidth > 1)
+            if (checkRotate.IsChecked == false)
             {
-                spacePerCircle = totalSizeField / (myWidth - 1);
+                myLength = int.Parse(myArray[0].Trim());
+                myWidth = int.Parse(myArray[1].Trim());
+
             }
-            else if(myWidth == 1)
+            else
             {
-                spacePerCircle = (float)(totalSizeField / myWidth);
+                myLength = int.Parse(myArray[1].Trim());
+                myWidth = int.Parse(myArray[0].Trim());
+
             }
 
-            float EllipseSize = (spacePerCircle) / EllipseDistance;
 
 
-            //int EllipseSize = 140;
+            if (myLength < 27 && myWidth < 27)
+            {
+                float totalSizeField = 840;
+                float EllipseDistance = 2F; //(float)(60.0 / EllipseSize)
 
-            int countAlphabet = -1;
-            int countAlphabet2 = 0;
+                if (myWidth > 1)
+                {
+                    spacePerCircle = totalSizeField / (myWidth - 1);
+                }
+                else if (myWidth == 1)
+                {
+                    spacePerCircle = (float)(totalSizeField / myWidth);
+                }
 
-            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                float EllipseSize = (spacePerCircle) / EllipseDistance;
 
-            //generates rows and colums of circles
 
-            for (int h = 0; h < myLength; h++)
+                //int EllipseSize = 140;
+
+                int countAlphabet = -1;
+                int countAlphabet2 = 0;
+
+                string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+                //generates rows and colums of circles
+
+                for (int h = 0; h < myLength; h++)
+                {
+
+                    for (int w = 0; w < myWidth; w++)
+                    {
+                        
+
+                        Ellipse ellipse = new Ellipse();
+
+                        ellipse.Fill = new SolidColorBrush(convertEmptyWells);
+                        ellipse.Height = EllipseSize;
+                        ellipse.Width = EllipseSize;
+                        //Debug.WriteLine($"n{alphabet[h]}_{myWidth - w - 1}");
+                        ellipse.Name = $"n{alphabet[h]}_{myWidth - w - 1}_{ellipseCounter}";
+
+                        ellipse.Margin = new Thickness(200, (h * (EllipseSize * EllipseDistance)), w * (EllipseSize * EllipseDistance), 200);
+
+                        gMyGrid.Children.Add(ellipse);
+
+
+                        //numeric labels
+                        if (h == 0)
+                        {
+                            Label lblCoordinateNum = new Label();
+                            lblCoordinateNum.FontSize = EllipseSize / EllipseDistance;
+                            lblCoordinateNum.Height = EllipseSize * 1.1;
+                            lblCoordinateNum.Width = EllipseSize;
+                            //lblCoordinateNum.Background = Brushes.Red;
+                            lblCoordinateNum.Content = myWidth - w - 1;
+
+                            lblCoordinateNum.Margin = new Thickness(0, -250, -205 + (w * EllipseSize * EllipseDistance) + 3, 0);
+                            lblCoordinateNum.Foreground = Brushes.Black;
+
+                            gLabelTest.Children.Add(lblCoordinateNum);
+
+                        }
+
+                        //alphabetic labels
+                        if (w == 0)
+                        {
+                            Label lblCoordinateAlphabet = new Label();
+                            lblCoordinateAlphabet.FontSize = EllipseSize * 0.66;
+                            lblCoordinateAlphabet.Height = EllipseSize * 1.33;
+                            lblCoordinateAlphabet.Width = EllipseSize * 1.5;
+
+                            if (countAlphabet > 25)
+                            {
+                                countAlphabet = 0;
+                                countAlphabet2 += 1;
+                            }
+                            else
+                            {
+                                countAlphabet += 1;
+                            }
+                            lblCoordinateAlphabet.Content = alphabet[countAlphabet2].ToString() + alphabet[countAlphabet].ToString();
+                            //lblCoordinateAlphabet.Background = Brushes.Aquamarine;
+                            lblCoordinateAlphabet.Margin = new Thickness(0, -202 + (h * EllipseSize * EllipseDistance), -400, 0);
+                            lblCoordinateAlphabet.Foreground = Brushes.Black;
+
+                            gLabelTest.Children.Add(lblCoordinateAlphabet);
+                        }
+
+                        ellipseCounter += 1;
+
+                    }
+                }
+
+                //Debug.WriteLine($"TC: {totalCircles}");
+                txbSize.Text = "";
+
+            }
+            else
             {
                 
-                for (int w = 0; w < myWidth; w++)
-                {
-                    
-                    Ellipse ellipse = new Ellipse();
-                    
-                    ellipse.Fill = new SolidColorBrush(convertEmptyWells);
-                    ellipse.Height = EllipseSize;
-                    ellipse.Width = EllipseSize;
-                    ellipse.Name = $"n{alphabet[h]}_{myWidth - w - 1}";
-                    
-                    ellipse.Margin = new Thickness(200, (h * (EllipseSize * EllipseDistance)), w * (EllipseSize * EllipseDistance), 200);
-                    
-                    gMyGrid.Children.Add(ellipse);
-
-
-                    //numeric labels
-                    if (h == 0)
-                    {
-                        Label lblCoordinateNum = new Label();
-                        lblCoordinateNum.FontSize = EllipseSize / EllipseDistance;
-                        lblCoordinateNum.Height = EllipseSize * 1.1;
-                        lblCoordinateNum.Width = EllipseSize;
-                        //lblCoordinateNum.Background = Brushes.Red;
-                        lblCoordinateNum.Content = myWidth - w - 1;
-
-                        lblCoordinateNum.Margin = new Thickness(0, -250, -205 + (w * EllipseSize * EllipseDistance) + 3, 0);
-                        lblCoordinateNum.Foreground = Brushes.Black;
-
-                        gLabelTest.Children.Add(lblCoordinateNum);
-
-                    }
-
-                    //alphabetic labels
-                    if (w == 0)
-                    {
-                        Label lblCoordinateAlphabet = new Label();
-                        lblCoordinateAlphabet.FontSize = EllipseSize * 0.66;
-                        lblCoordinateAlphabet.Height = EllipseSize * 1.33;
-                        lblCoordinateAlphabet.Width = EllipseSize * 1.5;
-                     
-                        if (countAlphabet > 25)
-                        {
-                            countAlphabet = 0;
-                            countAlphabet2 += 1;
-                        }
-                        else
-                        {
-                            countAlphabet += 1;
-                        }
-                        lblCoordinateAlphabet.Content = alphabet[countAlphabet2].ToString() + alphabet[countAlphabet].ToString();
-                        //lblCoordinateAlphabet.Background = Brushes.Aquamarine;
-                        lblCoordinateAlphabet.Margin = new Thickness(0, -202 + (h * EllipseSize * EllipseDistance), -400, 0);
-                        lblCoordinateAlphabet.Foreground = Brushes.Black;
-
-                        gLabelTest.Children.Add(lblCoordinateAlphabet);
-                    }
-
-                }
+                MessageBox.Show("Getal kan niet boven de 26 zijn");
             }
+            
 
-            //Debug.WriteLine($"TC: {totalCircles}");
-            txbSize.Text = "";
-
+            
         }
 
         private void addValue(object sender, RoutedEventArgs e)
